733b3d4c785d456420203f7a4167868ae7f39976
EricQSC <anacmmanulc@gmail.com>
2026-04-20 23:40:00 +0100

    Alterar mensagem

--- src/main/java/com/emlinha/App.java
+++ src/main/java/com/emlinha/App.java
@@ -11,6 +11,6 @@ package com.emlinha;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Olá a Ana, estou aqui para te ajudar!");
     }
 }
