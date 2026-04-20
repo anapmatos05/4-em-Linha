38db4f0c3ec18bfe7497c7c8eb3f2433ffb99c05
Ana Matos <ana.matos@ipvc.pt>
2026-04-20 17:24:37 +0100

    Commit inicial do projeto 4 em Linha

---
+++ src/main/java/com/emlinha/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.emlinha;
+
+/**
+ *
+ * @author anapm
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
